07f7a98f2f6aab9962a7cd365a36b2b920e36d23
CarlosCaoLopez <87083554+CarlosCaoLopez@users.noreply.github.com>
2023-04-12 03:26:53 +0200

    Add files via upload

---
+++ Gallaecia/src/gui/principal/PrincipalController.java
@@ -0,0 +1,5 @@
+package gui.principal;
+
+public class PrincipalController {
+
+}
--- Gallaecia/src/modelo/Main.java
+++ Gallaecia/src/modelo/Main.java
@@ -1,7 +1,9 @@
 package modelo;
 
+import gui.principal.Principal;
+
 public class Main {
     public static void main(String[] args) {
-
+        new Principal(args);
     }
 }
