0bb79164a1ddd0709bb70b6ce71e9aab9b879311
Yago-Falgueras <yago.falgueras@rai.usc.es>
2023-04-12 11:09:18 +0200

    Creacion de algunas clases, que son entidades de la BD

---
+++ Gallaecia/src/modelo/AtraccionFamiliar.java
@@ -0,0 +1,47 @@
+package modelo;
+
+import java.util.Objects;
+
+public class AtraccionFamiliar {
+    private Atraccion atraccion;
+    private Integer idadeRecomendada;
+
+    public AtraccionFamiliar(Atraccion atraccion, Integer idadeRecomendada) {
+        if(atraccion!=null){
+            this.atraccion = atraccion;
+        }
+        if(idadeRecomendada!=null){
+            this.idadeRecomendada = idadeRecomendada;
+        }
+    }
+
+    public Atraccion getAtraccion() {
+        return atraccion;
+    }
+
+    public Integer getIdadeRecomendada() {
+        return idadeRecomendada;
+    }
+
+
+    public void setAtraccion(Atraccion atraccion) {
+        this.atraccion = atraccion;
+    }
+
+    public void setIdadeMin(Integer idadeRecomendada) {
+        this.idadeRecomendada = idadeRecomendada;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AtraccionFamiliar)) return false;
+        AtraccionFamiliar that = (AtraccionFamiliar) o;
+        return atraccion.equals(that.atraccion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(atraccion);
+    }
+}
