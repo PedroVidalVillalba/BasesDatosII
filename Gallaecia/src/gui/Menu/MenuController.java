b82feb20a05b57720d415d198c3e1d645ae63e24
CarlosCaoLopez <caolopezcarlos@gmail.com>
2023-04-12 19:14:59 +0200

    menuControllerDone

--- Gallaecia/src/gui/Menu/MenuController.java
+++ Gallaecia/src/gui/Menu/MenuController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 
 public class MenuController {
     @FXML
-    private MenuBar menuBar;
+    private MenuBar menuBar;//NOTA: no basta con poner @FXML, necesitas en el fxml poner " fx:id="menuBar" "
 
     private Stage stage;
     private Scene scene;
@@ -25,40 +25,48 @@ public class MenuController {
 
 
     public void switchToLogin(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("../login/Login.fxml"));//Tenemos que indicar el paquete, si no, no encuentra la referencia
+        stage = (Stage) menuBar.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToRegister(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../login/Register.fxml"));
+        stage = (Stage) menuBar.getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
+
     public void switchToRide(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Ride.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("../general/Ride.fxml"));
+        stage = (Stage) menuBar.getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
     public void switchToRestaurant(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Restaurant.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("../general/Restaurant.fxml"));
+        stage = (Stage) menuBar.getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
     public void switchToRating(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Rating.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("../general/Rating.fxml"));
+        stage = (Stage) menuBar.getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
     public void switchToShow(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Show.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("../general/Show.fxml"));
+        stage = (Stage) menuBar.getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
