19fa863f33318695f75c903908177741b46ad7ca
iggarbayo <iggarbayo@gmail.com>
2023-04-12 12:27:49 +0200

    cambio de la barra de menú. Se ha creado el paquete Menú con su controlador y su fmxl que se añadirá mediante FMXLLoader a cada ventana. Cada escena se crea mediante un VBox del menú y la escena root. Este comando las apila verticalmente.

--- Gallaecia/src/gui/principal/Principal.java
+++ Gallaecia/src/gui/principal/Principal.java
@@ -2,10 +2,17 @@ package gui.principal;
 
 import javafx.application.Application;
 import baseDatos.BaseDatos;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Principal extends Application {
@@ -20,12 +27,16 @@ public class Principal extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
+
         //FXMLLoader loader = new FXMLLoader(this.getClass().getResource("Principal.fxml"));
         Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+        Parent menu = FXMLLoader.load(getClass().getResource("../Menu/menu.fxml"));
 
-        Scene scene = new Scene(root);//Necesita un nodo (hay muchos tipos)
+        VBox mainContainer = new VBox(menu,root);
 
-       // String css = this.getClass().getResource("Principal.css").toExternalForm();
+        Scene scene = new Scene(mainContainer);//Necesita un nodo (hay muchos tipos)
+
+        // String css = this.getClass().getResource("Principal.css").toExternalForm();
        // scene.getStylesheets().add(css);
 
         stage.setScene(scene);//Seteamos la escena jjj
