7152f8807e7591ce586b5bccf114802d65dac797
iggarbayo <iggarbayo@gmail.com>
2023-04-18 12:12:53 +0200

    inicio de video

--- Gallaecia/src/gui/principal/SceneController.java
+++ Gallaecia/src/gui/principal/SceneController.java
@@ -1,20 +1,55 @@
 package gui.principal;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 import javafx.event.ActionEvent;
+
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Clase para cambiar de escena
  *
  */
-public class SceneController {
+public class SceneController implements Initializable {
+
+    @FXML
+    private MediaView mediaView;
+
+    private File file;
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File file = new File("src/gui/principal/video.mp4");
+        media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+        mediaPlayer.play();
+    }
+
+    public void playMedia() {
+
+    }
+
+    public void resetMedia() {
+
+    }
+
 }
 
     //Ride = Atracción
