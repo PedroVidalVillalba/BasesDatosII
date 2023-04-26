ab17ad1cbcfa886ad5fdc20ab4e4c2bcc2c9ffe1
Pedro Vidal Villalba <pedrovidalvillalba@gmail.com>
2023-04-26 20:47:40 +0200

    Puesta la barra del menú bonita

--- Gallaecia/src/gui/admin/AdminController.java
+++ Gallaecia/src/gui/admin/AdminController.java
@@ -1,24 +1,14 @@
 package gui.admin;
 
-import baseDatos.DataBase;
 import gui.SceneManager;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import modelo.User;
 
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class AdminController implements Initializable {
